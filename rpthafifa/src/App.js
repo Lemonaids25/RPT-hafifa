47c9c392a44cb54f9ec98daf31a82bb8e1f43f56
unknown <Shahamitay@gmail.com>
2025-08-27 14:36:44 +0300

    added asseys and compass

--- rpthafifa/src/App.js
+++ rpthafifa/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import ExampleComponent from './components/ExampleComponent';
+import degreePng from '/public/360-degrees.png'; // Make sure the file exists in src/
 
 function App() {
-    return (
-        <div>
-            <h1>Welcome to My React App</h1>
-            <ExampleComponent />
-        </div>
-    );
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: '#fff'
+    }}>
+      <img
+        src="/360-degrees.png"
+        alt="360 degree markings"
+        style={{ maxWidth: '90vw', maxHeight: '90vh' }}
+      />
+    </div>
+  );
 }
 
 export default App;
